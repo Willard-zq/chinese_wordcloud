686b8bb9e28972458a5bfcf171a07a1f6123a6b9
mantuoluozk <1741695059@qq.com>
2018-12-14 14:06:04 +0800

    中文词云生成

---
+++ codes/run.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from wordcloud import WordCloud, ImageColorGenerator
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import jieba
+from os import path
+
+# 文本路径名
+text_path = 'txt/apple.txt' 
+
+# 中文字体路径名
+font_path = 'txt/Hiragino.ttf' 
+
+# 背景图片路径名
+img_mask = 'imgs/apple.png'
+
+# 保存的图片名字
+img_result = 'result.png' 
+
+# 获取当前文件路径
+p = path.dirname('.')
+
+# 打开文本
+text = open(path.join(p, text_path),'r', encoding='UTF-8').read()
+
+# 中文分词
+text = ' '.join(jieba.cut(text))
+print(text[:100])
+
+# 生成对象
+mask = np.array(Image.open(img_mask))
+wc = WordCloud(mask=mask, font_path=font_path, mode='RGBA', background_color=None).generate(text)
+
+# 从图片中生成颜色
+image_colors = ImageColorGenerator(mask)
+wc.recolor(color_func=image_colors)
+
+# 显示词云
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+# 保存到文件
+wc.to_file(path.join(p, img_result))
